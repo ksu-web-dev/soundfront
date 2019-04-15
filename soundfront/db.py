ac2a8888009b5f40cb127fbb60a415f2c0f2f526
Derek <ddinh@users.noreply.github.com>
2019-04-14 21:02:01 -0500

    Modify Database to store connection instead of cursor

--- soundfront/db.py
+++ soundfront/db.py
@@ -4,21 +4,19 @@ import os
 
 class Database:
 
-	def __init__(self):
-		# ideally, get these params from config
-		self.server = os.environ.get('DB_SERVER', default='localhost,1433')
-		self.database = os.environ.get('DB_DATABASE', default='TestDB')
-		self.username = os.environ.get('DB_USERNAME', default='SA')
-		self.password = os.environ.get('DB_PASSWORD', default='')
-		
-		# if self.password is None:
-			# throw errror
+    def __init__(self):
+        # ideally, get these params from config
+        self.server = os.environ.get('DB_SERVER', default='localhost,1433')
+        self.database = os.environ.get('DB_DATABASE', default='TestDB')
+        self.username = os.environ.get('DB_USERNAME', default='SA')
+        self.password = os.environ.get('DB_PASSWORD', default='')
 
-	def connect(self):
-		cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
-		self.cursor = cnxn.cursor()
+    def connect(self):
+        self.conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server +
+                                   ';DATABASE='+self.database+';UID='+self.username+';PWD=' + self.password)
 
-	def get_version(self):
-		self.cursor.execute('select @@version')
-		row = self.cursor.fetchone()
-		return row[0] 
+    def get_version(self):
+        cursor = self.conn.cursor()
+        cursor.execute('select @@version')
+        row = cursor.fetchone()
+        return row[0]
