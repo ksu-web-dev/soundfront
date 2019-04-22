40fa1731f79c4561e98d6c77d25a00dd68765157
Derek <ddinh@users.noreply.github.com>
2019-04-21 20:06:49 -0500

    Fix indentation

--- soundfront/album.py
+++ soundfront/album.py
@@ -2,6 +2,7 @@ from flask import (Blueprint, render_template)
 
 bp = Blueprint('album', __name__, url_prefix='/album')
 
+
 class AlbumRepo():
     def __init__(self, conn):
         self.conn = conn
@@ -18,14 +19,14 @@ class AlbumRepo():
             """, user_id, album_title, album_length, album_price, album_description)
         return cursor.fetchone()
 
-	def list_songs(self, album_id):
-		cursor = self.conn.cursor()
-		cursor.exectue("""
+        def list_songs(self, album_id):
+            cursor = self.conn.cursor()
+            cursor.exectue("""
 		EXEC Soundfront.GetAlbumSongs
 			@AlbumID=?
 			""", album_id)
-		return cursor.fetchall()
-	
+            return cursor.fetchall()
+
     def get_album(self, id):
         cursor = self.conn.cursor()
         cursor.execute('EXEC Soundfront.ReadAlbum @AlbumAlbumId=?', id)
