1b4a296a0c238b1ddd5b743fd2a45e9e8dba06e3
Derek <ddinh@users.noreply.github.com>
2019-04-22 18:45:17 -0500

    Add album title to song list query

--- sql/song.sql
+++ sql/song.sql
@@ -62,7 +62,8 @@ CREATE OR ALTER PROCEDURE Soundfront.ListSong
 AS
 
 SELECT S.SongID, S.UserID, S.AlbumID, S.Title, S.[Length],
-	S.UploadDate, S.Price, S.[Description], U.DisplayName as Artist
+	S.UploadDate, S.Price, S.[Description], U.DisplayName as Artist,
+	A.Title as AlbumTitle
 FROM Soundfront.Song S
 	LEFT JOIN Soundfront.Album A ON A.AlbumID = S.AlbumID
 	INNER JOIN Soundfront.[User] U ON U.UserID = U.UserID
--- tests/test_song.py
+++ tests/test_song.py
@@ -2,6 +2,7 @@ import unittest
 from soundfront.db import Database
 from soundfront.user import UserRepo
 from soundfront.song import SongRepo
+from soundfront.album import AlbumRepo
 
 
 class TestSong(unittest.TestCase):
@@ -11,15 +12,19 @@ class TestSong(unittest.TestCase):
         cls.db = db
         db.connect()
         cls.user_repo = UserRepo(db.conn)
+        cls.album_repo = AlbumRepo(db.conn)
         cls.repo = SongRepo(db.conn)
 
     def test_insert_song(self):
         user = TestSong.user_repo.create_user(
             email='email', display_name='user', password='test')
 
+        album = TestSong.album_repo.create_album(
+            user_id=user.UserID, album_title='some album', album_price=9.99, album_description='')
+
         song = TestSong.repo.insert_song(
             userid=user.UserID,
-            albumid=None,
+            albumid=album.AlbumID,
             title='some song',
             length=230,
             price=1,
@@ -33,3 +38,4 @@ class TestSong(unittest.TestCase):
         self.assertEqual(len(songs), 1)
         self.assertEqual(songs[0].Title, 'some song')
         self.assertEqual(songs[0].Artist, 'user')
+        self.assertEqual(songs[0].AlbumTitle, 'some album')
